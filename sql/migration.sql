818bb39c859fbd961eb2bf4178c8043662679f0c
Septian Dika Maulana <99572215+Septian567@users.noreply.github.com>
2025-10-08 21:45:26 +0700

    Update migration.sql

--- sql/migration.sql
+++ sql/migration.sql
@@ -2,9 +2,10 @@ CREATE DATABASE IF NOT EXISTS todolist_db CHARACTER SET utf8mb4 COLLATE utf8mb4_
 USE todolist_db;
 
 CREATE TABLE IF NOT EXISTS todos (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  id CHAR(36) PRIMARY KEY,
   title VARCHAR(255) NOT NULL,
   description TEXT,
   is_done TINYINT(1) DEFAULT 0,
   created_at DATETIME NOT NULL
 );
+
